78b5fdef846b8020162e1becf4d70a1783be2edb
p-junseo <belsono47@gmail.com>
2025-08-02 20:36:25 +0900

    Update main.ts

--- markdown-connect/src/main.ts
+++ markdown-connect/src/main.ts
@@ -1,21 +1,14 @@
+import './style.css'
 import { createMarkdownRenderer } from './markdown'
-import { renderLineChart } from './renderLineChart.ts'
+import { renderLineChart } from './renderLineChart'
 
-const markdownText = `\`\`\`dsl
-title: 친구 키
-type: line
-x: 이름
-y: 키
-data:
-민호,180
-민서,173
-주현,168
-\`\`\``
+// 마크다운 텍스트 불러오기
+import markdownText from '../docs/test.md?raw'
 
 const md = createMarkdownRenderer()
 document.querySelector('#app')!.innerHTML = md.render(markdownText)
 
-// 마운트 후 D3 렌더링
+// DSL 차트 렌더링
 document.querySelectorAll('.dsl-chart').forEach((el) => {
   const chartData = JSON.parse(el.getAttribute('data-chart')!)
   renderLineChart(el as HTMLElement, chartData)
